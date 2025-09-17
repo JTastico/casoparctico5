5a052b920fc37560ae26c7238a3d2e4c55f65e5c
JTastico <jamilturpoarocutipa@gmail.com>
2025-09-17 18:39:56 -0500

    conection string

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
+using Casopractico.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+// CONECTION STRING
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<ConsultoriaDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
